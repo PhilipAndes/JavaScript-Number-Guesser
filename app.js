6f1e837f3d71c81705b142f81238b8fcff671d54
Philip Andes <philip__andes@hotmail.nl>
2019-05-01 14:03:49 +0200

    Game function description added, global vars defined

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+GAME FUNCTION:
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let player choose to play again
+*/
+
+// Game values
+//We first write down all the vars, this time we going to define them different, you don't need to write a semicolon everytime but instead you can use a comma
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;

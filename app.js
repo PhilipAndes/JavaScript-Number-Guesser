3442e3d363a1c648e400a6609c7a884081215260
Philip Andes <philip__andes@hotmail.nl>
2019-05-01 14:59:35 +0200

    cleaner code with the gameOver function, but you still see the original code I commented out

--- app.js
+++ app.js
@@ -39,14 +39,44 @@ guessBtn.addEventListener('click', function(){
 
     // Check if it is the winning number
     if(guess === winningNum){
-        // Disable input if correct
-        guessInput.disabled === true;
-        // Change the border to green if correct
-        guessInput.style.borderColor = 'green';
-        // Set Message if correct
-        setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
-    } else {
+        // Game over - won
 
+        // // Disable input if correct, commented everything below out because its better to write a function for this as we did on the bottom (gameOver function) to make the code look cleaner
+        // guessInput.disabled === true;
+        // // Change the border to green if correct
+        // guessInput.style.borderColor = 'green';
+        // // Set Message if correct
+        // setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+        gameOver(true, `${winningNum} is correct, YOU WIN!`);
+
+    } else {
+        // Wrong number 
+        guessesLeft -= 1;
+
+        //Then we want to check if there are any guesses left, so we say if guessesLeft equals zero it means they lost
+        if(guessesLeft === 0){
+            //Game over - lost
+
+            // Disable input if game over, same as above, I have commented everything out as we can write the code much cleaner with the gameOver function:
+            // guessInput.disabled === true;
+            // // Change the border to red if game over
+            // guessInput.style.borderColor = 'red';
+            // // Set Message if game over
+            // setMessage(`Game Over, you lost. The correct number was ${winningNum}`, 'red');
+
+            gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
+        } else {
+            // Game continues - answer wrong
+
+            // Change the border to red if wrong answer
+            guessInput.style.borderColor = 'red';
+
+            // Clear the input
+            guessInput.value = '';
+
+            // Tell user its the wrong number
+            setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+        }
     }
 });
 
@@ -55,4 +85,21 @@ function setMessage(msg, color){
     //We want the text to be red when it is an error or green when it's good, so we set another parameter above in the function named color and set it in the message above to red. 
     message.style.color = color;
     message.textContent = msg;
+}
+
+// Game Over
+function gameOver(won, msg){
+    let color;
+    //So we say if won is equal to true then the color is green else the color is red:
+    won === true ? color = 'green' : color = 'red';
+
+
+    // Disable input
+    guessInput.disabled === true;
+    // Change the border color
+    guessInput.style.borderColor = color;
+    // Set text color
+    message.style.color = color;
+    // Set Message if correct
+    setMessage(msg);
 }
